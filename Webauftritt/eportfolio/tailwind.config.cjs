a4f4be4761236166fc339bf0c9b053cc711d77a7
Tobias Hilfiker <tobias.hilfiker@edu.gbssg.ch>
2022-12-07 15:49:40 +0100

    Build index.html, create nav-component, add colors and handlebar-plugin to config

--- Webauftritt/eportfolio/tailwind.config.cjs
+++ Webauftritt/eportfolio/tailwind.config.cjs
@@ -1,17 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}"],
-  theme: {
-    extend: {
-      colors: {
-        primary: '#003249',
-        secondary: '#007EA7'
-      },
-      fontSize: {
-        's-h1': '50px',
-        h1: '56px'
-      }
+    content: ["./*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                textcolor: '#CCDBDC',
+                navbackground: '#003249',
+                primary: '#9AD1D4',
+                banner: '#007EA7'
+            },
+            fontSize: {
+                's-h1': '50px',
+                h1: '56px'
+            }
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
