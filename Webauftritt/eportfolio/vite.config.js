e75a47e52855f0a38654b35f6d12fddfc4c48c0e
Tobias Hilfiker <tobias.hilfiker@edu.gbssg.ch>
2022-12-21 22:40:24 +0100

    fix export problems

--- Webauftritt/eportfolio/vite.config.js
+++ Webauftritt/eportfolio/vite.config.js
@@ -1,7 +1,17 @@
-import { resolve } from 'path';
+import {resolve} from 'path';
 import handlebars from 'vite-plugin-handlebars';
 
 export default {
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                rules: resolve(__dirname, 'rules.html'),
+                impressions: resolve(__dirname, 'impressions.html'),
+                spiritofcurling: resolve(__dirname, 'spiritofcurling.html'),
+            },
+        }
+    },
     plugins: [handlebars({
         partialDirectory: resolve(__dirname, 'partials'),
     })],
